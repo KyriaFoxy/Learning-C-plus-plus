e7db30ee9e85e05d38f73623cba61b5ea9a6a94e
Astryda Malinowska <astamalinowska@outlook.com>
2023-01-11 18:32:34 +0100

    Created first program in C++

---
+++ 2_1_myfirst.cpp
@@ -0,0 +1,20 @@
+//printing text
+
+#include <iostream>
+int main()
+{
+	using namespace std;
+	cout << "Hello, World!" << endl; //for keeping an old tradition
+	cout << "Let's play with C++ language!";
+	cout << endl;
+	cout << "It will be great adventure!" << endl;
+
+	return 0;
+}
+
+//Output:
+/*
+Hello, World!
+Let's play with C++ language!
+It will be great adventure!
+*/
